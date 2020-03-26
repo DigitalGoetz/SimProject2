ea5e6371ee3ea01ae43ded6015ebfaf3fa42ad85
Mike Goetz <digital.goetz@gmail.com>
2020-03-26 01:50:25 -0400

    cleaned up remaining data

---
+++ analysis/scripts/preprocess_arrivals.py
@@ -0,0 +1,51 @@
+import os
+import json
+import sys
+import requests
+import datetime
+import csv
+from elasticsearch import Elasticsearch
+import numpy as np
+
+es = Elasticsearch([{'host': 'localhost', 'port': '9000'}])
+es.indices.delete(index='arrivals', ignore=[400, 404])
+
+for file in os.listdir("arrival"):
+    with open("arrival/" + file) as dataset:
+        lastvalue = None
+        arrivaltimes = []
+        rdr = csv.reader(dataset, delimiter=' ', quotechar='|')
+
+        for row in rdr:
+            if row[0] != "\xef\xbb\xbf":
+                line = "".join(row)
+                values = line.strip().split(":")
+                timevalue = datetime.time(hour=int(values[0]), minute=int(values[1]), second=int(values[2]))
+                timestamp = datetime.datetime.combine(datetime.date.today(), timevalue)
+
+                if lastvalue is None:
+                    lastvalue = timestamp
+                else:
+                      difference = timestamp - lastvalue
+                      lastvalue = timestamp
+                      arrivaltimes.append(round(difference.total_seconds() / 60.0, 2))
+    
+        dataset_report = {}
+        # get mean, median, standard deviation, min and max
+        dataset_report['mean'] = round(np.mean(arrivaltimes),2)
+        dataset_report['median'] = round(np.median(arrivaltimes),2)
+        dataset_report['standard_deviation'] = round(np.std(arrivaltimes),2)
+        dataset_report['min'] = round(np.amin(arrivaltimes),2)
+        dataset_report['max'] = round(np.amax(arrivaltimes),2)
+        dataset_report['data'] = arrivaltimes
+        dataset_report['source'] = file
+
+        # For CSV
+        #print(str(dataset_report['mean']) + ',' + str(dataset_report['median']) + ',' + str(dataset_report['standard_deviation']) + ',' + str(dataset_report['min']) + ',' + str(dataset_report['max'])+ ',' + file)
+        # For Console perusal
+        #print('mean:' +str(dataset_report['mean']) + ' \tmed:' + str(dataset_report['median']) + ' \tstd:' + str(dataset_report['standard_deviation']) + ' \tmin:' + str(dataset_report['min']) + ' \tmax:' + str(dataset_report['max'])+ ' \tsrc:' + file)
+        es.index(index='arrivals', body=dataset_report)
+                  
+search = es.search(index='arrivals', size=5000)
+
+print(json.dumps(search))
---
+++ analysis/scripts/process_arrivals.py
@@ -0,0 +1,31 @@
+import json
+from elasticsearch import Elasticsearch
+import numpy as np
+import plotly.express as px
+import pandas as pd
+
+es = Elasticsearch([{'host': 'localhost', 'port': '9000'}])
+search = es.search(index='arrivals', size=5000)
+hitarray = search['hits']['hits']
+
+dataset = []
+for hit in hitarray:
+    data = hit["_source"]["data"]
+    for datum in data:
+        dataset.append(datum)
+
+dataset_report = {}
+# get mean, median, standard deviation, min and max
+dataset_report['mean'] = round(np.mean(dataset),2)
+dataset_report['median'] = round(np.median(dataset),2)
+dataset_report['standard_deviation'] = round(np.std(dataset),2)
+dataset_report['min'] = round(np.amin(dataset),2)
+dataset_report['max'] = round(np.amax(dataset),2)
+dataset.sort()
+dataset_report['data'] = dataset
+
+#print(json.dumps(dataset_report))
+
+df = pd.DataFrame(dict(Times=dataset))
+fig = px.histogram(df, x="Times", labels={'Times' : 'Arrival Times'}, nbins=35)
+fig.show()
